12de5d12197cb1180548e5a710658927c3075fd2
Gabriel Costa Cerqueira <gabrielcerqueira96@hotmail.com>
2021-06-02 16:04:26 -0300

    feat: route adapter middleware

--- app/infrastructure/http/middlewares/route_adapter.go
+++ app/infrastructure/http/middlewares/route_adapter.go
@@ -1,1 +1,32 @@
 package middlewares
+
+import (
+	"encoding/json"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/hlog"
+	"github.com/sptGabriel/banking/app/presentation/responses"
+	"net/http"
+)
+
+func Handle(handler func(r *http.Request) responses.Response) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger := hlog.FromRequest(r)
+
+		res := handler(r)
+		if res.Error != nil {
+			logger.Error().Err(res.Error)
+		}
+
+		WriteResponse(w, res.Data, res.Status, logger)
+	}
+}
+
+func WriteResponse(w http.ResponseWriter, d interface{}, st int, l *zerolog.Logger) {
+	w.Header().Set("content-Type", "application/json")
+	w.WriteHeader(st)
+	if d != nil {
+		if err := json.NewEncoder(w).Encode(d); err != nil {
+			l.Error().Stack().Err(err).Msg("failed to encode response")
+		}
+	}
+}
