da78cacc522a4a83af126bb7206afd3e7a25b15f
Gabriel Costa Cerqueira <gabrielcerqueira96@hotmail.com>
2021-06-02 16:04:53 -0300

    refactor: string to uuid util

--- app/utils/uuid.go
+++ app/utils/uuid.go
@@ -2,10 +2,10 @@ package utils
 
 import "github.com/google/uuid"
 
-func ToUUID (id string) uuid.UUID {
+func ToUUID(id string) uuid.UUID {
 	uuid, err := uuid.Parse(id)
 
-	if err !=nil {
+	if err != nil {
 
 	}
 
