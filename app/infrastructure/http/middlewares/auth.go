97214aa9f15783a576675346ae9d96b515aa6ee6
Gabriel Costa Cerqueira <gabrielcerqueira96@hotmail.com>
2021-06-04 13:07:56 -0300

    refactor: authorization middleware

--- app/infrastructure/http/middlewares/auth.go
+++ app/infrastructure/http/middlewares/auth.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"context"
 	"github.com/rs/zerolog/hlog"
+	"github.com/sptGabriel/banking/app"
 	"github.com/sptGabriel/banking/app/application/ports"
 	"github.com/sptGabriel/banking/app/presentation/responses"
 	"net/http"
@@ -14,8 +15,8 @@ func AuthHandle(cipherService ports.CipherService,next http.HandlerFunc) http.Ha
 		l := hlog.FromRequest(r)
 		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
 		if len(authHeader) != 2 {
-			msg := responses.Error{Message: "Malformed token"}
-			responses.WriteResponse(w,msg, http.StatusUnauthorized, l)
+			err := app.ErrMalformedToken
+			responses.WriteResponse(w,responses.Error{Message:err.Error()} , http.StatusBadRequest, l)
 			return
 		}
 
@@ -23,8 +24,8 @@ func AuthHandle(cipherService ports.CipherService,next http.HandlerFunc) http.Ha
 
 		claim, err := cipherService.Decrypt(jwtToken)
 		if err != nil {
-			msg := responses.Error{Message: "Malformed token"}
-			responses.WriteResponse(w,msg, http.StatusUnauthorized, l)
+			err := app.ErrUnauthorized
+			responses.WriteResponse(w,responses.Error{Message:err.Error()} , http.StatusUnauthorized, l)
 			return
 		}
 
